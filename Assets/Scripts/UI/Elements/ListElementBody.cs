6737898236f8652bbb63a95f81b93ed1defbcd98
lihaozheng <1176467960@qq.com>
2026-03-28 15:44:44 +0800

    更新： 1.右侧面板添加常驻WTinCAT面板，可SET 2.右侧面板变量块，可GET 新增： 1.WTinCAT面板脚本 2.显隐外壳方法

--- Assets/Scripts/UI/Elements/ListElementBody.cs
+++ Assets/Scripts/UI/Elements/ListElementBody.cs
@@ -1,4 +1,5 @@
-using game4automation;
+﻿using game4automation;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,52 @@ public class ListElementBody : BaseElement
         base.Initialization(signal);
 
     }
-    
-    
+
+    private void Start()
+    {
+        StartCoroutine("UpdateVariableData");
+        
+    }
+
+    /// <summary>
+    /// 数据更新
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator UpdateVariableData() 
+    {
+        while (true) 
+        {
+            boolValue.SetIsOnWithoutNotify(ThisSignal.GetValue().Equals(true));
+            Elementfloat.text = ThisSignal.GetValue().ToString();
+            Elementint.text = ThisSignal.GetValue().ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }
+    public void OnSetElementValue(object value) 
+    {
+        switch (signalType)
+        {
+            case SignalType.InputBool:
+                ThisSignal.SetValue(Convert.ToBoolean(value));
+                break;
+            case SignalType.InputInt:
+                ThisSignal.SetValue(Convert.ToInt32(value));
+                break;
+            case SignalType.InputFloat:
+                ThisSignal.SetValue(Convert.ToSingle(value));
+                break;
+            case SignalType.OutputBool:
+                ThisSignal.SetValue(Convert.ToBoolean(value));
+                break;
+            case SignalType.OutputInt:
+                ThisSignal.SetValue(Convert.ToInt32(value));
+                break;
+            case SignalType.OutputFloat:
+                ThisSignal.SetValue(Convert.ToSingle(value));
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/UI/Elements/TwinCATinterfaceElment.cs
@@ -0,0 +1,128 @@
+﻿using game4automation;
+using MathNet.Numerics;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using static game4automation.Game4AutomationBehavior;
+using static game4automation.TwinCatAdsInterface;
+
+public class TwinCATinterfaceElment : MonoBehaviour
+{
+    public Dropdown dropdownActive;
+    public InputField inputFieldID;
+    public InputField inputFieldPort;
+    public Dropdown dropdownMode;
+    public InputField inputFieldCommands;
+    public Toggle toggleDeBug;
+    private int Activetag;
+    private int Modetag;
+
+    public Button RemoveAllSignal;
+    public Button AddAllSignal;
+    
+    public TwinCatAdsInterface TwinCatAdsInterface;
+
+    private void Start()
+    {
+        RemoveAllSignal.onClick.AddListener(() => { TwinCatAdsInterface.DestroyAllSignals();    });
+        RemoveAllSignal.onClick.AddListener(() => { 
+            TwinCatAdsInterface.ImportAllSignals();
+            //TwinCatAdsInterface.ImportSignals(true);
+        });
+        GetTwinCatAllDate();
+        //StartCoroutine("UpdateGetTwinCatAllDate") ;
+        dropdownActive.onValueChanged.AddListener(SetTwinCATActive);
+        dropdownMode.onValueChanged.AddListener(SetTwinCatUpdateMode);
+        toggleDeBug.onValueChanged.AddListener(SetDebugMod);
+
+        
+    }
+    /// <summary>
+    /// 设置连接状态
+    /// </summary>
+    /// <param name="tag"></param>
+    public void SetTwinCATActive(int tag) 
+    {
+        switch (tag)
+        {
+            case 0: TwinCatAdsInterface.Active=ActiveOnly.Always;   break;
+            case 1: TwinCatAdsInterface.Active=ActiveOnly.Connected; break;
+            case 2: TwinCatAdsInterface.Active=ActiveOnly.Disconnected; break;
+            case 3: TwinCatAdsInterface.Active=ActiveOnly.Never; break;
+            case 4: TwinCatAdsInterface.Active=ActiveOnly.DontChange; break;
+        };
+    }
+    /// <summary>
+    /// 设置更新模式
+    /// </summary>
+    /// <param name="tag"></param>
+    public void SetTwinCatUpdateMode(int tag) 
+    {
+        switch (tag)
+        {
+            case 0: TwinCatAdsInterface.UpdateMode= updatemode.Cyclic; break;
+            case 1: TwinCatAdsInterface.UpdateMode = updatemode.OnChange; break;
+            case 2: TwinCatAdsInterface.UpdateMode = updatemode.CyclicSumCommand; break;
+        };
+    }
+
+    public void SetID() 
+    {
+        TwinCatAdsInterface.NetId = inputFieldID.text;
+    }
+    public void SetPort()
+    {
+        TwinCatAdsInterface.Port = Convert.ToInt32(inputFieldPort.text);
+    }
+    public void SetCommands() 
+    {
+        TwinCatAdsInterface.MaxNumberADSSubCommands= Convert.ToInt32(inputFieldCommands.text);
+    }
+    public void SetDebugMod(bool open) 
+    {
+        Debug.LogWarning(open);
+        TwinCatAdsInterface.DebugMode = toggleDeBug.enabled;
+    }
+    IEnumerator UpdateGetTwinCatAllDate() 
+    {
+        while (!inputFieldID.isFocused&& !inputFieldPort.isFocused&& !inputFieldCommands.isFocused) 
+        {
+            GetTwinCatAllDate();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    /// <summary>
+    /// 获取ADS初始化信息
+    /// </summary>
+    public void  GetTwinCatAllDate() 
+    {
+        //act
+        switch (TwinCatAdsInterface.Active) 
+        {
+            case ActiveOnly.Always: Activetag = 0; break;
+            case ActiveOnly.Connected: Activetag = 1; break;
+            case ActiveOnly.Disconnected: Activetag = 2; break;
+            case ActiveOnly.Never: Activetag = 3; break;
+            case ActiveOnly.DontChange: Activetag = 4; break;
+        } ;
+        dropdownActive.value = Activetag;
+        //id
+        inputFieldID.text = TwinCatAdsInterface.NetId;
+        //port
+        inputFieldPort.text = TwinCatAdsInterface.Port.ToString();
+        //mode
+        switch (TwinCatAdsInterface.UpdateMode)
+        {
+            case updatemode.Cyclic: Modetag = 0; break;
+            case updatemode.OnChange: Modetag = 1; break;
+            case updatemode.CyclicSumCommand: Modetag = 2; break;
+        };
+        dropdownMode.value = Modetag;
+        //maxads
+        inputFieldCommands.text= TwinCatAdsInterface.MaxNumberADSSubCommands.ToString();
+        //debug
+        toggleDeBug.SetIsOnWithoutNotify(TwinCatAdsInterface.DebugMode);
+    }
+}
